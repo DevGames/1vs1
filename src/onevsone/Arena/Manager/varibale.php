1aa7c1da0a0bb69b6a5df320b2868c3e99633157
Nawaf_Craft1b <Nawafmg97@gmail.com>
2016-10-10 04:00:27 -0700

    Update varibale.php

--- src/onevsone/Arena/Manager/varibale.php
+++ src/onevsone/Arena/Manager/varibale.php
@@ -1,1 +1,23 @@
+<?php
 
+namespace Arena\Manager\varibale;
+
+class varibale {
+ 
+  public $round = array();
+  
+  public $time = array();
+  
+  #public $reload = array();
+  
+  public $player = array();
+  
+  public function checkRound($round){
+   
+    array_map(function($m)
+              {if($m !== $round)
+              {return;}
+              },$this->round);
+  }
+  
+}
