5990b4564d08e37cb89b9649df7f4fcddf558e01
Nawaf_Craft1b <Nawafmg97@gmail.com>
2016-10-12 11:35:09 -0700

    Update varibale.php

--- src/onevsone/Arena/Manager/varibale.php
+++ src/onevsone/Arena/Manager/varibale.php
@@ -8,7 +8,10 @@ class Variable extends Player {
     public $namedtag = array();
     
     public function Variable(){
+       if($this->namedtag["reload"] > 0){
+       unset($this->namedtag["game"]);
        return array_map(function($k){if(count($k) == 0){$this->namedtag["info"] = "nawaf";}}, $this->namedtag);
+       }
     }
 }
 ?>
