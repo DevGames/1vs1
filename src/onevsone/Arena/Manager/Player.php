697cd19f41dad25de62e41fa34efed1912e8c547
Nawaf_Craft1b <Nawafmg97@gmail.com>
2016-10-12 11:39:37 -0700

    Update Player.php

--- src/onevsone/Arena/Manager/Player.php
+++ src/onevsone/Arena/Manager/Player.php
@@ -1,1 +1,17 @@
+<?php
 
+namespace onevsone\Arena\Manager;
+
+class Player extends Variable {
+   
+    public function add(){
+        return
+        $this->namedtag["game"]["player"][$this->namedtag["name"]] = $this->namedtag["name"];
+    }
+    
+    public function remove(){
+        unset($this->namedtag["game"]["player"][$this->namedtag["name"]]);
+    }
+    
+}
+?>
