142e62460023756d939b6c5f0c850445fe571e13
Nawaf_Craft1b <Nawafmg97@gmail.com>
2016-10-13 07:51:15 -0700

    Update Player.php

--- src/onevsone/Arena/Manager/Player.php
+++ src/onevsone/Arena/Manager/Player.php
@@ -5,7 +5,9 @@ namespace onevsone\Arena\Manager;
 class Player extends Variable {
    
     public function add(){
-        return
+        $b = "";
+        $this->getServer()->getPluginManager()->callEvent(new \onevsone\root\GameEvent\JoinGameEvent\JoinGameEvent($this->getServer()->getPlayer($this->namedtag["name"]),$b,$this->namedtag["game"]));
+        $this->getServer()->getPlayer($this->namedtag["name"])->sendMessage($b);
         $this->namedtag["game"]["player"][$this->namedtag["name"]] = $this->namedtag["name"];
     }
     
