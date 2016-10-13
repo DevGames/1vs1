179ef0a77cadca67119188e7ca51b8b76f03f826
Nawaf_Craft1b <Nawafmg97@gmail.com>
2016-10-13 07:50:10 -0700

    Update JoinGameEvent.php

--- src/onevsone/root/GameEvent/JoinGameEvent.php
+++ src/onevsone/root/GameEvent/JoinGameEvent.php
@@ -1,1 +1,31 @@
+<?php
 
+namespace onevsone\root\GameEvent\JoinGameEvent;
+  
+
+class JoinGameEvent extends \pocketmine\event\Event implements \pocketmine\event\Cancellable {
+
+    public function __construct($player,$msg,$game) {
+        $this->msg = $msg;
+        $this->player = $player;
+        $this->game = $game;
+    }
+    
+    public function getMessage(){
+        return $this->msg;
+    }
+    
+    public function getPlayer(){
+        return $this->player;
+    }
+    
+    public function setMessage(){
+        return $this->msg;
+    }
+    
+    public function getGame(){
+        return $this->game;
+    }
+    
+}
+?>
