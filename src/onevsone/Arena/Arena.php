c2b7cd7861b0202a148a641b4f3673884245f5bc
Nawaf_Craft1b <Nawafmg97@gmail.com>
2016-10-13 06:19:53 -0700

    Update Arena.php

--- src/onevsone/Arena/Arena.php
+++ src/onevsone/Arena/Arena.php
@@ -1,1 +1,17 @@
+<?php
 
+namespace onevsone\Arena;
+  
+
+class Arena extends Manager\Stats {
+
+    public function getArena(){
+        return $this->namedtag["game"];
+    }
+    
+    public function reloadArena(){
+        return clone (new Manager\Variable())->namedtag["reload"] = PHP_INT_SIZE;
+    }
+    
+}
+?>
